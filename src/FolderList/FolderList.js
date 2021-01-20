e5da7f81db5b44c66eb735842ddacef2cde46759
Jonah Tarver <jonahdevine@gmail.com>
2021-01-19 19:41:45 -0800

    Added note count for folders

--- src/FolderList/FolderList.js
+++ src/FolderList/FolderList.js
@@ -7,9 +7,16 @@ class FolderList extends Component {
     static contextType = NoteContext;
     render() {
         const folders = this.context.folders.map(folder => {
+            let noteCount =  0
+            this.context.notes.forEach(note => {
+                if(note.folder === folder.id){
+                    noteCount++
+                }
+            })
             return (
                 <li key={folder.id}>
                     <NavLink to={`/folder/${folder.id}`}>{folder.title}</NavLink>
+                    <p>Total Notes: {noteCount}</p>
                 </li>
             )
         })
